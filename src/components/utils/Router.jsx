3156100fa9b93ace6fbc7a99682c8ff9142c313e
Nikita Fomin <defdXs@yandex.ru>
2025-03-11 20:49:17 +0300

    HOLY JESUS IT WORKS

--- src/components/utils/Router.jsx
+++ src/components/utils/Router.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { SamplesPage } from '../../react-envelope/components/pages/SamplesPage/SamplesPage';
+import { AuthPage } from '../../react-envelope/components/pages/AuthPage/AuthPage';
 
 export const Router = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<SamplesPage/>}/>
+                <Route path="/auth" element={<AuthPage/>}/>
             </Routes>
         </BrowserRouter>
     );
--- src/index.js
+++ src/index.js
@@ -5,11 +5,17 @@ import './react-envelope/variables.css'
 import './react-envelope/styles.css'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { AuthProvider } from './react-envelope/contexts/AuthContext';
+import { AccountsProvider } from './react-envelope/contexts/AccountsContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App/>
+    <AuthProvider>
+      <AccountsProvider>
+        <App/>
+      </AccountsProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
 
