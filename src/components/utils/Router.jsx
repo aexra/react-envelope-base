48a0d00c312477c522d8466a66be4c5075a489d2
Nikita Fomin <defdXs@yandex.ru>
2025-03-13 15:10:38 +0300

    New routes

--- src/components/utils/Router.jsx
+++ src/components/utils/Router.jsx
@@ -1,6 +1,9 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { SamplesPage } from '../../react-envelope/components/pages/SamplesPage/SamplesPage';
 import { AuthPage } from '../../react-envelope/components/pages/AuthPage/AuthPage';
+import { PrivateRoute } from "../../react-envelope/utils/PrivateRoute";
+import { DevExpPage } from "../../react-envelope/components/pages/DevExpPage/DevExpPage";
+import { UserSettingsPage } from "../../react-envelope/components/pages/UserSettingsPage/UserSettingsPage";
 
 export const Router = () => {
     return (
@@ -8,6 +11,12 @@ export const Router = () => {
             <Routes>
                 <Route path="/" element={<SamplesPage/>}/>
                 <Route path="/login" element={<AuthPage/>}/>
+                <Route element={<PrivateRoute/>}>
+                    <Route path="/_lab" element={<DevExpPage/>}/>
+                </Route>
+                <Route path="/profile">
+                    <Route path="settings" element={<UserSettingsPage/>}/>
+                </Route>
             </Routes>
         </BrowserRouter>
     );
